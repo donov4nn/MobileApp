a4786b98b7f4fd0c9dd2cadf0fca7e44cb93acb9
Donovan <dono.m51@gmail.com>
2023-09-09 02:50:49 +0200

    Character script and scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var anim = get_node("AnimationPlayer")
+@onready var sprite = get_node("AnimatedSprite2D")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,15 +14,44 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		setCharacterDirection(direction)
+		if velocity.y == 0:
+			run()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if velocity.y == 0:
+			idle()
+	
+	if velocity.y > 0:
+		fall()
+		
 	move_and_slide()
+
+# moves
+func fall():
+	anim.play("Fall")
+
+func idle():
+	anim.play("Idle")
+
+func jump():
+	anim.play("Jump")
+
+func run():
+	anim.play("Run")
+	
+# to make the character move from left to right	
+func setCharacterDirection(direction):
+	if direction == -1:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false

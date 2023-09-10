66bec9e7290415f3477e0f99fbb07f371f7910a5
Donovan <dono.m51@gmail.com>
2023-09-10 04:47:50 +0200

    Logic on the draw line

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,59 +1,53 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300
+@export var jump_force = 220
+@export var gravity = 300
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var anim = get_node("AnimationPlayer")
-@onready var sprite = get_node("AnimatedSprite2D")
+@onready var ap = get_node("AnimationPlayer")
+@onready var sprite = get_node("Sprite2D")
 
 func _physics_process(delta):
 	
-	# Add the gravity.
-	if not is_on_floor():
+	# gravity
+	if not is_on_floor():  
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jump()
+	if Input.is_action_just_pressed("Jump") && is_on_floor():
+		velocity.y = -jump_force
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("Left", "Right")
+	var horizontal_direction = Input.get_axis("Left", "Right")
+	velocity.x = horizontal_direction * speed
 	
-	if direction:
-		velocity.x = direction * SPEED
-		set_character_direction(direction)
-		
-		if velocity.y == 0:
-			run()
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
-			idle()
-	
-	if velocity.y > 0:
-		fall()
-		
+	if horizontal_direction != 0:
+		set_character_direction(horizontal_direction)
+
 	move_and_slide()
+	update_animations(horizontal_direction)
+
+func update_animations(horizontal_direction):
+	if is_on_floor() && horizontal_direction == 0:
+		idle()
+	elif is_on_floor():
+		run()
+	elif velocity.y < 0:
+		jump()
+	else:
+		fall()
 
 # moves
 func fall():
-	anim.play("Fall")
+	ap.play("fall")
 
 func idle():
-	anim.play("Idle")
+	ap.play("idle")
 
 func jump():
-	anim.play("Jump")
+	ap.play("jump")
 
 func run():
-	anim.play("Run")
+	ap.play("run")
 
-# to make the character move from left to right	
 func set_character_direction(direction):
-	if direction == -1:
-		sprite.flip_h = true
-	else:
-		sprite.flip_h = false
+	sprite.flip_h = (direction == -1)

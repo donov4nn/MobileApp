3629c1e404e2a47c1d37227168f264fa9b4c7101
Donovan <dono.m51@gmail.com>
2023-09-11 04:54:50 +0200

    no message

--- Scripts/Util.gd
+++ Scripts/Util.gd
@@ -14,3 +14,32 @@ static func delete_children(node):
 	for n in node.get_children():
 		node.remove_child(n)
 		n.queue_free()
+
+# algo pour rendre plus smooth les lignes que l'utilisateur va dessiner
+# https://fr.wikipedia.org/wiki/Algorithme_de_trac%C3%A9_de_segment_de_Bresenham
+static func bresenham(start, end) -> Array[Vector2]:
+	var points : Array[Vector2] = []
+	var x0 = int(start.x)
+	var y0 = int(start.y)
+	var x1 = int(end.x)
+	var y1 = int(end.y)
+
+	var dx = abs(x1 - x0)
+	var dy = abs(y1 - y0)
+	var sx = 1 if x0 < x1 else -1
+	var sy = 1 if y0 < y1 else -1
+	var err = dx - dy
+
+	while true:
+		points.append(Vector2(x0, y0))
+		if x0 == x1 and y0 == y1:
+			break
+		var e2 = 2 * err
+		if e2 > -dy:
+			err -= dy
+			x0 += sx
+		if e2 < dx:
+			err += dx
+			y0 += sy
+
+	return points
